06421157f2b844edc90ec3e28d30df67a8abcd30
Erin <erin.wiggers@lyntonweb.com>
2017-05-06 10:39:46 -0600

    gulp-sass and gulp added

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,11 @@
-gulpfile.js
+var gulp = require('gulp');
+var sass = require('gulp-sass')
+
+gulp.task('sass', function() {
+	return gulp.src('src/scss/app.scss')
+		.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+		.pipe(gulp.dest('app/css'));
+});
+
+gulp.task('default', ['sass']);
 
